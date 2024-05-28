39209e1ed9244784f82182b55c458f400842c017
Moonquit <clash.of.clans64836@gmail.com>
2024-05-28 19:08:54 +0300

    Выровнял отрисовку

--- Main.cpp
+++ Main.cpp
@@ -1,32 +1,32 @@
-#include <iostream>
+п»ї#include <iostream>
 #include <vector>
 #include <cctype>
 #include <stdexcept> 
 using namespace std;
 
+
 void drawBoard(const vector<vector<char>>& board) {
     cout << "   A   B   C\n";
+    cout << "  +---+---+---+\n";
     for (int i = 0; i < 3; ++i) {
-        cout << " " << i + 1 << " ";
+        cout << i + 1 << " |";
         for (int j = 0; j < 3; ++j) {
-            cout << " " << board[i][j] << " ";
-            if (j < 2) cout << "|";
+            cout << " " << board[i][j] << " |";
         }
-        cout << endl;
-        if (i < 2) cout << "  ---|---|---\n";
+        cout << "\n";
+        cout << "  +---+---+---+\n";
     }
 }
-
 pair<int, int> convertMove(const string& move) {
     if (move.length() != 2) {
-        throw invalid_argument("Недійсний формат переміщення. Використовуйте формат '1A', '2B', etc.");
+        throw invalid_argument("РќРµРґС–Р№СЃРЅРёР№ С„РѕСЂРјР°С‚ РїРµСЂРµРјС–С‰РµРЅРЅСЏ. Р’РёРєРѕСЂРёСЃС‚РѕРІСѓР№С‚Рµ С„РѕСЂРјР°С‚ '1A', '2B', etc.");
     }
 
     int row = move[0] - '1';
     int col = toupper(move[1]) - 'A';
 
     if (row < 0 || row >= 3 || col < 0 || col >= 3) {
-        throw invalid_argument("Вийти за межі.");
+        throw invalid_argument("Р’РёР№С‚Рё Р·Р° РјРµР¶С–.");
     }
 
     return { row, col };
